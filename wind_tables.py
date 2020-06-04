c2ba8b08c48676c81d75d7dab010b6ae987d6340
CloudyOverhead <jerome.simon@hotmail.com>
2020-06-04 18:24:41 -0400

    Make wind probabilities the same magnitude

--- wind_tables.py
+++ wind_tables.py
@@ -9,6 +9,7 @@ K_WIND = np.array(
 D_WIND = np.array(
     [.0, .0, .0, 13, .0, .0, .0, .0, .0, 8.5, 34, 8.5, .0, .0, .0, .0]
 )
+D_WIND = D_WIND / 10  # To make winds the same magnitude.
 
 S_WIND = np.array(
     [.0, .0, .1, .1, .0, .0, .0, .0, .1, 2.2, 4.3, 3.8, 2.5, .8, .2, .0]
