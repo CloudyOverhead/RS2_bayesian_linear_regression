4a49918fab59247847490ff946555d95b3135526
CloudyOverhead <jerome.simon@hotmail.com>
2020-06-10 12:51:45 -0400

    Bayesian inference: working and refactored

--- bayesian_inference.py
+++ bayesian_inference.py
@@ -1,76 +1,254 @@
-import numpy as np
-from matplotlib import pyplot as plt
+from os.path import join
+from copy import copy
 
-from prepare_variables import get_variables, SITE_WIND
-from wind_tables import ANGLES
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+from prepare_variables import get_variables
+
+ANGLE_SITE = {
+    'S': 6,
+    'D': 2,
+    'K': 14,
+}
+FIGURE_PATH = "figures"
+
+
+def get_posterior(vars, xs, y):
+    *as_, std = vars
+    n = len(as_) + 2
+
+    for i, a in enumerate(as_):
+        new_shape = np.ones(n, dtype=int)
+        new_shape[i] = -1
+        as_[i] = a.reshape(new_shape)
+
+    new_shape = np.ones(n, dtype=int)
+    new_shape[-2] = -1
+    std = std.reshape(new_shape)
+
+    new_shape = np.ones(n, dtype=int)
+    new_shape[-1] = -1
+    xs = [x.reshape(new_shape) for x in xs]
+    y = y.reshape(new_shape)
+
+    posterior = gaussian(
+        x=y,
+        mean=np.sum([a*x for a, x in zip(as_, xs)], axis=0),
+        std=std,
+        n=len(as_),
+    )
+    posterior = np.prod(posterior, axis=-1)
+
+    return posterior
+
+
+def get_stats(posterior, vars, null_dims, print_=True, problem=None):
+    argmax = np.argmax(posterior)
+    unravel_argmax = list(np.unravel_index(argmax, posterior.shape))
+    prob_max = posterior.flatten()[argmax]
+    # prob_uniform = posterior.mean()
+    for dim in sorted(null_dims)[::-1]:
+        posterior = np.moveaxis(posterior, dim, -1)
+        idx_null = np.argmin(np.abs(vars[dim]))
+        prob_null = posterior[..., idx_null]
+    prob_null = prob_null.max()
+    vars_max = [var[unravel_argmax[i]] for i, var in enumerate(vars)]
+    probs_max = get_max_distribution(unravel_argmax, posterior)
+    std_max = [weighted_std(var, prob) for var, prob in zip(vars, probs_max)]
+
+    if print_:
+        print(" "*3, f"{problem}:")
+        print(" "*7, "Best fit mean:", vars_max)
+        print(" "*7, "Best fit std:", std_max)
+        # print(" "*7, "Rapport à l'uniforme:", prob_max / prob_uniform)
+        print(" "*7, "Rapport à l'hypothèse nulle:", prob_max / prob_null)
+
+    return argmax, unravel_argmax, vars_max, probs_max, std_max
+
+
+def weighted_std(values, weights):
+    average = np.average(values, weights=weights)
+    variance = np.average((values-average)**2, weights=weights)
+    return np.sqrt(variance)
+
+
+def get_max_distribution(argmax, posterior):
+    distributions = []
+    for i, (var, name) in enumerate(zip(vars, var_names)):
+        temp_argmax = copy(argmax)
+        temp_argmax[i] = ...
+        distributions.append(posterior[temp_argmax])
+    return distributions
+
+
+def gaussian(x, mean, std, n=1):
+    return (
+        np.exp(-((x-mean)/std)**2 / 2)
+        / (np.sqrt(2*np.pi) * std)**n
+    )
+
+
+def gaussian_fill_between(a, b, std, xlim=None, ylim=None):
+    if xlim is None:
+        xlim = plt.xlim()
+    if ylim is None:
+        ylim = plt.ylim()
+
+    # for s, a in zip(range(1, 4), [.25, .125, .0625]):
+    #     plt.fill_between(
+    #         p,
+    #         line-s*snow_noise,
+    #         line+s*snow_noise,
+    #         alpha=a,
+    #         color="tab:blue",
+    #     )
+    #     plt.imshow(gaussian_fill_between)
+
+    extent = [*xlim, *ylim]
+    x, y = np.meshgrid(np.linspace(*xlim, 1000), np.linspace(*ylim, 1000))
+    fill_between = gaussian(y, a*x+b, std)
+
+    tab_blue = mpl.colors.to_rgb(mpl.colors.BASE_COLORS["k"])
+    alpha = np.linspace(0, .3, 1000)
+    cmap = mpl.colors.ListedColormap([[*tab_blue, a] for a in alpha])
+    plt.imshow(
+        fill_between,
+        origin="lower",
+        extent=extent,
+        aspect="auto",
+        cmap=cmap,
+    )
+
+
+def plot_linear_dependency(problem, x, y, a, b, std, xlabel="", ylabel=""):
+    plt.title(f"Site {site}")
+    plt.scatter(x, y, s=4, c="k")
+    extend = products.max() - products.min()
+    x_line = np.linspace(x.min()-extend, x.max()+extend, 2)
+    line = a*x_line + b
+    gaussian_fill_between(a, b, std)
+    plt.autoscale(False)
+    plt.plot(x_line, line, ls='--', c="k")
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.savefig(join(FIGURE_PATH, f"Results_{problem}_{site}"))
+    plt.show()
+
+
+def plot_parameters(site, problem, vars, var_names, probs_max):
+    fig, axes = plt.subplots(1, len(vars), sharey=True)
+    axes[0].set_ylabel("Probability (normalized to maximum)")
+    for i, (var, name) in enumerate(zip(vars, var_names)):
+        ax = axes.flatten()[i]
+        width = np.diff(var)
+        probs_max_ = probs_max[i] / probs_max[i].max()
+        ax.bar(var, probs_max_, [*width, width[-1]], color=[.3, .3, .3])
+        ax.set_xlabel(name)
+        ax.set_xlim([var.min(), var.max()])
+        if i == len(vars)-1:
+            ax.set_xscale('log')
+    plt.tight_layout()
+    plt.savefig(join(FIGURE_PATH, f"Parameters_{problem}_{site}"))
+    plt.show()
 
 
 if __name__ == "__main__":
+    STEPS = 16  # Remplacer par 32 pour plus de définition.
+
     for site, data, products in get_variables():
-        STEPS = 16
+        print(f"Site {site}")
+        # data = np.concatenate([data.values[:, [2, 3]], products[:, ANGLE_SITE[site], None]], axis=1)
+        # sns.pairplot(
+        #     pd.DataFrame(data, columns=["ice", "snow", "wind"])
+        # )
+        # plt.show()
 
-        snow_0 = np.linspace(-3, 3, STEPS)
-        product_dep = np.linspace(-3, 3, STEPS)
-        angles_prob = SITE_WIND[site]
-        snow_noise = np.logspace(-.5, .5, STEPS)
-        ice_0 = np.linspace(-3, 3, STEPS)
-        snow_dep = np.linspace(-3, 3, STEPS)
-        velocity_dep = np.linspace(-3, 3, STEPS)
-        ice_noise = np.logspace(-.5, .5, STEPS)
+        snow, ice, vv = data[["snow", "ice", "VV"]].values.T
+        products = products[:, ANGLE_SITE[site]]
 
-        hypotheses = [
-            snow_0, product_dep, angles_prob, snow_noise,
-            ice_0, snow_dep, velocity_dep, ice_noise,
+        """Snow"""
+
+        product_dep = np.linspace(.0, .25, STEPS)
+        snow_0 = np.linspace(-.5, .5, STEPS)
+        snow_noise = np.logspace(-1.0, .25, STEPS)
+        vars = [product_dep, snow_0, snow_noise]
+        var_names = ["Shore-wind product", "S0", "sigma_S"]
+
+        posterior = get_posterior(vars, [products, np.ones_like(snow)], snow)
+        argmax, unravel_argmax, vars_max, probs_max, std_max = get_stats(
+            posterior, vars, null_dims=[0], problem="Snow",
+        )
+
+        plot_linear_dependency(
+            "snow",
+            products,
+            snow,
+            *vars_max,
+            xlabel="Shore-wind product",
+            ylabel="Snow",
+        )
+        plot_parameters(site, "snow", vars, var_names, probs_max)
+
+        """Ice"""
+
+        snow_dep = np.linspace(-1.0, 0.0, STEPS)
+        ice_0 = np.linspace(-.5, .5, STEPS)
+        ice_noise = np.logspace(-1.0, .25, STEPS)
+        vars = [snow_dep, ice_0, ice_noise]
+        var_names = ["Snow dependency", "I0", "sigma_I"]
+
+        posterior = get_posterior(vars, [snow, np.ones_like(ice)], ice)
+        argmax, unravel_argmax, vars_max, probs_max, std_max = get_stats(
+            posterior, vars, null_dims=[0], problem="Ice",
+        )
+
+        plot_linear_dependency(
+            "ice",
+            snow,
+            ice,
+            *vars_max,
+            xlabel="Snow",
+            ylabel="Ice",
+        )
+        plot_parameters(site, "ice", vars, var_names, probs_max)
+
+        """VV"""
+
+        vv_snow_dep = np.linspace(-.5, .5, STEPS)
+        vv_ice_dep = np.linspace(-.5, .5, STEPS)
+        vv_0 = np.linspace(-.5, .5, STEPS)
+        vv_noise = np.logspace(-1.0, 0, STEPS)
+        vars = [vv_snow_dep, vv_ice_dep, vv_0, vv_noise]
+        var_names = [
+            "Snow dependency",
+            "Ice dependency",
+            "sigma_VV0",
+            "sigma_sigma_VV",
         ]
-        for i, h in enumerate(hypotheses):
-            new_shape = np.ones(9, dtype=int)
-            new_shape[i] = STEPS
-            h = h.astype(np.float16)
-            hypotheses[i] = h.reshape(new_shape)
-        (
-            snow_0, product_dep, angles_prob, snow_noise,
-            ice_0, snow_dep, velocity_dep, ice_noise
-        ) = hypotheses
 
-        products = products.T
-        products = products.reshape(
-            [1, 1, products.shape[0], 1, 1, 1, 1, 1, products.shape[1]]
+        posterior = get_posterior(
+            vars,
+            [snow, ice, np.ones_like(vv)],
+            vv,
         )
-        products = products.astype(np.float32)
-
-        snow, ice, velocity = data.loc[:, ["snow", "ice", "velocity"]].values.T
-        new_shape = np.ones(9, dtype=int)
-        new_shape[-1] = len(data)
-        snow = snow.reshape(new_shape).astype(np.float32)
-        ice = ice.reshape(new_shape).astype(np.float32)
-        velocity = velocity.reshape(new_shape).astype(np.float32)
-
-        posterior_snow = (
-            np.exp(
-                -(snow-snow_0-product_dep*products)**2
-                / (2*snow_noise**2)
-            )
-            / snow_noise
+        argmax, unravel_argmax, vars_max, probs_max, std_max = get_stats(
+            posterior, vars, null_dims=[0], problem="Ice",
         )
-        posterior_snow = angles_prob[..., 0] * np.prod(posterior_snow, axis=-1)
 
-        argmax = np.nanargmax(posterior_snow)
-        print(argmax, posterior_snow.flatten()[argmax])
-
-        posterior_ice = (
-            np.exp(
-                -(ice-ice_0+snow_dep*snow+velocity_dep*velocity)**2
-                / (2*ice_noise**2)
-            )
-            / ice_noise
+        vv_snow_dep, vv_ice_dep, vv_0, vv_noise = vars_max
+        plot_linear_dependency(
+            "vv_to_snow",
+            snow,
+            vv-vv_ice_dep*ice,
+            a=vv_snow_dep,
+            b=vv_0,
+            std=vv_noise,
+            xlabel="Snow",
+            ylabel="sigma_VV - Ice",
         )
-        posterior_ice = np.prod(posterior_ice, axis=-1)
-
-        argmax = np.nanargmax(posterior_ice)
-
-        posterior_hypotheses = posterior_snow * posterior_ice
-
-        argmax = np.nanargmax(posterior_hypotheses)
-        print(argmax, posterior_hypotheses.flatten()[argmax])
-
-        raise
+        plot_parameters(site, "ice", vars, var_names, probs_max)
+        break
